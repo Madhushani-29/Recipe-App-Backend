a3a595a803a7178d0240dc9bff46ef1339cee44a
madhushani-29 <madhushani2904@gmail.com>
2024-07-17 03:44:17 +0530

    package.json changed

--- src/server.js
+++ src/server.js
@@ -16,8 +16,8 @@ app.use(express.json());
 
 app.use(cors());
 
-app.get("/health", async (req, res) => {
-    res.status(200).json({ message: "Health is ok" });
+app.get("/", async (req, res) => {
+    res.status(200).json({ message: "Server running correctly!" });
   });
 
   
