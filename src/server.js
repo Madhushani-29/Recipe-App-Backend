9df893865c169027bfbeb849d222b10036f0d0da
madhushani-29 <madhushani2904@gmail.com>
2024-07-17 02:14:39 +0530

    health api added

---
+++ src/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import {connectDB} from "./Connection.js";
+import {errorHandler} from "./Middleware/ErrorHandler.js";
+import {userRouter} from "./Router/UserRoutes.js";
+import {recipeRouter} from "./Router/RecipeRoutes.js";
+
+dotenv.config();
+const port=process.env.PORT || 3001;
+
+connectDB();
+const app=express();
+//to parse the json data from 
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/health", async (req, res) => {
+    res.status(200).json({ message: "Health is ok" });
+  });
+
+  
+app.use("/users", userRouter);
+app.use("/recipe", recipeRouter);
+app.use(errorHandler);
+
+
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`);
+});
