7196ed496249afb025bac2fa2b184b9fb8a8df2a
madhushani-29 <madhushani2904@gmail.com>
2024-07-17 22:17:43 +0530

    vercerl.json file removed

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/server.js',
+  output: {
+    filename: 'server.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  target: 'node',
+};
