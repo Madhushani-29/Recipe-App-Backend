c17eac72c2361447f4f7d46b5dd5457a7c2bde49
madhushani-29 <madhushani2904@gmail.com>
2024-07-18 00:55:32 +0530

    refactored code

--- src/index.js
+++ src/index.js
@@ -16,11 +16,7 @@ const app=express();
 //to parse the json data from 
 app.use(express.json());
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World')
