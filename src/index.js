802d609862636612a8b596698ff811d385b2f5de
madhushani-29 <madhushani2904@gmail.com>
2024-07-18 00:22:54 +0530

    code refactorings

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import {connectDB} from "./src/Connection.js";
-import {errorHandler} from "./src/Middleware/ErrorHandler.js";
-import {userRouter} from "./src/Router/UserRoutes.js";
-import {recipeRouter} from "./src/Router/RecipeRoutes.js";
+import {connectDB} from "./Connection.js";
+import {errorHandler} from "./Middleware/ErrorHandler.js";
+import {userRouter} from "./Router/UserRoutes.js";
+import {recipeRouter} from "./Router/RecipeRoutes.js";
 
 dotenv.config();
 const port=process.env.PORT || 3001;
